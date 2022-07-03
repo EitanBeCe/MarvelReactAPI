02b8a95d821c57c0d9bc8c9efe3d0d7625ee7ad8
Eitan <eitanleviberger@gmail.com>
2022-07-03 16:51:35 +0300

    minor comment added

--- src/components/appBanner/AppBanner.js
+++ src/components/appBanner/AppBanner.js
@@ -3,7 +3,9 @@ import avengers from '../../resources/img/Avengers.png';
 import avengersLogo from '../../resources/img/Avengers_logo.png';
 
 const AppBanner = () => {
+    
     return (
+        
         <div className="app__banner">
             <img src={avengers} alt="Avengers"/>
             <div className="app__banner-text">
